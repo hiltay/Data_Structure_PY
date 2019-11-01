7587ab0871c817f41548f227023cf5d9f489e2eb
hiltay <904108079@qq.com>
2019-11-01 21:50:28 +0800

    "add" operation

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -2,7 +2,8 @@
 # -*- coding:utf-8 -*-
 
 
-# 二叉搜索树
+# 二叉搜索树,不含重复元素
+# todo 包含重复元素的二叉搜索树
 class BST:
     # 定义节点
     class __Node:
@@ -22,3 +23,19 @@ class BST:
     # 是否为空树
     def is_empty(self):
         return self.__size == 0
+
+    # 添加元素
+    def add(self, e,):
+        self.__root = self.__add(self.__root, e)
+
+    # 以node为根添加元素e
+    def __add(self, node, e):
+        if node is None:
+            self.__size += 1
+            return self.__Node(e)
+
+        if e < node.elem:
+            node.lchild = self.__add(node.lchild, e)
+        if e > node.elem:
+            node.rchild = self.__add(node.rchild, e)
+        return node
