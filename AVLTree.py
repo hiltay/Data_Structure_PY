abc1b1f5c29d19cb61739ae9d4e3869b882fe47d
hiltay <904108079@qq.com>
2019-11-19 20:24:23 +0800

    update

--- AVLTree.py
+++ AVLTree.py
@@ -37,6 +37,35 @@ class AVLTree:
     def is_empty(self):
         return self.__size == 0
 
+    # 当前树是否是二叉搜索树
+    def is_bst(self):
+        keys = []  # 存储各个结点的key
+        self.__inorder(self.__root, keys)
+        for i in range(len(keys) - 1):
+            if keys[i] > keys[i + 1]:
+                return False
+        return True
+
+    # 以node为根进行中序遍历
+    def __inorder(self, node, keys):
+        if node:
+            self.__inorder(node.lchild, keys)
+            keys.append(node.key)
+            self.__inorder(node.rchild, keys)
+
+    # 当前树是否是平衡二叉树
+    def is_balance(self):
+        return self.__is_balance(self.__root)
+
+    # node结点为根的树是否是平衡二叉树
+    def __is_balance(self, node):
+        if node is None:
+            return True
+        balancefactor = self.__get_balance_factor(node)
+        if abs(balancefactor) > 1:
+            return False
+        return self.__is_balance(node.lchild) and self.__is_balance(node.rchild)
+
     # 添加key,value
     def add(self, key, value):
         self.__root = self.__add(self.__root, key, value)
@@ -49,7 +78,7 @@ class AVLTree:
 
         if key < node.key:
             node.lchild = self.__add(node.lchild, key, value)
-        if key > node.key:
+        elif key > node.key:
             node.rchild = self.__add(node.rchild, key, value)
         else:  # key == node.key,树中已存在该key,修改value
             node.value = value
@@ -57,9 +86,62 @@ class AVLTree:
         node.height = 1 + max(self.__get_height(node.lchild), self.__get_height(node.rchild))
         # 计算平衡因子
         balancefactor = self.__get_balance_factor(node)
-        if abs(balancefactor) > 1:
-            pass  # todo 自平衡操作
+
+        """维护平衡"""
+        # 不平衡发生在向该结点左侧的左侧添加结点时(LL),对该结点右旋
+        if balancefactor > 1 and self.__get_balance_factor(node.lchild) >= 0:
+            return self.__right_rotate(node)
+        # 不平衡发生在向该结点左侧的右侧添加结点时(LR),对该结点的左孩子左旋,再对该节点右旋
+        if balancefactor > 1 and self.__get_balance_factor(node.lchild) < 0:
+            node.lchild = self.__left_rotate(node.lchild)
+            return self.__right_rotate(node)
+        # 不平衡发生在向该结点右侧的左侧添加结点时(RL),对该结点的右孩子右旋,再对该节点左旋
+        if balancefactor < -1 and self.__get_balance_factor(node.rchild) > 0:
+            node.rchild = self.__right_rotate(node.rchild)
+            return self.__left_rotate(node)
+        # 不平衡发生在向该结点右侧的右侧添加结点时(RR),对该结点左旋
+        if balancefactor < -1 and self.__get_balance_factor(node.rchild) <= 0:
+            return self.__left_rotate(node)
+
         return node
+
+    # 右旋,返回新树的根
+    #                  y                                        x
+    #                /   \                                    /   \
+    #              x       T4            右旋               z       y
+    #            /   \               ------------>        /  \    /  \
+    #           z     T3                                T1    T2 T3  T4
+    #         /  \
+    #       T1    T2
+    def __right_rotate(self, y):
+        x = y.lchild
+        T3 = x.rchild
+        # 向右旋转
+        x.rchild = y
+        y.lchild = T3
+        # 更新height,先y后x
+        y.height = 1 + max(self.__get_height(y.lchild), self.__get_height(y.rchild))
+        x.height = 1 + max(self.__get_height(x.lchild), self.__get_height(x.rchild))
+        return x
+
+    # 左旋,返回新树的根
+    #                  y                                        x
+    #                /   \                                    /   \
+    #              T1     x              左旋               y       z
+    #                   /   \        ------------>        /  \    /  \
+    #                 T2     z                           T1  T2  T3   T4
+    #                       /  \
+    #                     T3    T4
+    def __left_rotate(self, y):
+        x = y.rchild
+        T2 = x.lchild
+        # 向左旋转
+        x.lchild = y
+        y.rchild = T2
+        # 更新height,先y后x
+        y.height = 1 + max(self.__get_height(y.lchild), self.__get_height(y.rchild))
+        x.height = 1 + max(self.__get_height(x.lchild), self.__get_height(x.rchild))
+        return x
 
     # 返回node为根结点的树中,key所在的结点
     def __getnode(self, node, key):
